bc539c14963f69bb04cf950f8bdafd8fdf6a3a1d
itsjakk3 <itsjakk3@gmail.com>
2022-08-30 22:45:59 +0900

    Add project files.

---
+++ ViewSetMaker/ClViewSetMaker.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using ViewSetMaker.Ctr;
+using Autodesk.Navisworks.Api.Plugins;
+using System.Windows.Forms;
+
+/// <summary>
+/// Name
+/// </summary>
+namespace ViewSetMaker
+{
+    /// <summary>
+    /// Addin Info
+    /// </summary>
+    [Plugin("ViewSetMaker", "Keeleys95", DisplayName = "ViewSetMaker")]
+    [RibbonLayout("AddinRibbon.xaml")]
+    [RibbonTab("ID_CustomTab_1", DisplayName = "ViewSetMaker")]
+    [Command("ID_Button_1", Icon = "1_16.png", LargeIcon = "1_32.png", ToolTip = "Create Viewpoint List at once")]
+    ///Command(buttonId at XAML button, name of Icon IMG, name of Icon IMG, ToolTips)
+
+    /// Define Addin tab
+    public class ClViewSetMaker : CommandHandlerPlugin
+    {
+        public override int ExecuteCommand(string name, params string[] parameters)
+        {
+            switch (name)
+            {
+                case "ID_Button_1":
+                    if (!Autodesk.Navisworks.Api.Application.IsAutomated)
+                    {
+                        var pluginRecord = Autodesk.Navisworks.Api.Application.Plugins.FindPlugin("ViewSetMakerDockPanel.Keeleys95"); //'Namespace of tab'.'DeveloperName'
+                        
+                        if (pluginRecord is DockPanePluginRecord && pluginRecord.IsEnabled)
+                        {
+                            var docPanel = (DockPanePlugin)(pluginRecord.LoadedPlugin ?? pluginRecord.LoadPlugin()); // Load plugin when PluginRecord.LoadedPlugin is Null
+                            docPanel.ActivatePane();
+                        }
+
+                    }
+
+                    break;
+            }
+
+            return 0;
+        }
+    }
+}
+
+
+namespace AddinDockPanel
+{
+    [Plugin("ViewSetMakerDockPanel", "Keeleys95", DisplayName = "ViewSetMaker")] //Define Plugin's name, developer, display name
+    [DockPanePlugin(235, 500, AutoScroll = true, MinimumHeight = 500, MinimumWidth = 235)]
+
+    public class ViewSetMakerDockPanel : DockPanePlugin
+    {
+        public override Control CreateControlPane()
+        {
+            var tc = new TabControl();
+            tc.ParentChanged += SetDockStile;
+
+            var tp1 = new TabPage("Make Viewpoints");
+            tp1.Controls.Add(new UcViewMake());
+            tc.TabPages.Add(tp1);
+
+            return tc;
+        }
+
+        private void SetDockStile(object sender, EventArgs e)
+        {
+            try
+            {
+                var tc = sender as TabControl;
+                tc.Dock = DockStyle.Fill;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public override void DestroyControlPane(Control pane)
+        {
+            try
+            {
+                var ctr = (UcViewMake)pane;
+                ctr?.Dispose();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ ViewSetMaker/Ctr/UcViewMake.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace ViewSetMaker.Ctr
+{
+    partial class UcViewMake
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbAddinName = new System.Windows.Forms.Label();
+            this.txtTemplateName = new System.Windows.Forms.TextBox();
+            this.lbTemplate = new System.Windows.Forms.Label();
+            this.btNewSet = new System.Windows.Forms.Button();
+            this.btLoadSet = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btSaveAs = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.brProgress = new System.Windows.Forms.ProgressBar();
+            this.numBuildingStart = new System.Windows.Forms.TextBox();
+            this.lbDongNum = new System.Windows.Forms.Label();
+            this.lbBuildingStart = new System.Windows.Forms.Label();
+            this.lbBuildingEnd = new System.Windows.Forms.Label();
+            this.numBuildingEnd = new System.Windows.Forms.TextBox();
+            this.lbStandardFloor = new System.Windows.Forms.Label();
+            this.numStandardFloor = new System.Windows.Forms.TextBox();
+            this.lbEntranceFloor = new System.Windows.Forms.Label();
+            this.lbPlanPositionFloor = new System.Windows.Forms.Label();
+            this.numEntranceFloor = new System.Windows.Forms.TextBox();
+            this.lbUndergroundFloorNum = new System.Windows.Forms.Label();
+            this.lbPlanPositionFloorUnderground = new System.Windows.Forms.Label();
+            this.numUndergroundNum = new System.Windows.Forms.TextBox();
+            this.btRun = new System.Windows.Forms.Button();
+            this.lbDeveloperName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbAddinName
+            // 
+            this.lbAddinName.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.lbAddinName.Location = new System.Drawing.Point(3, 3);
+            this.lbAddinName.Name = "lbAddinName";
+            this.lbAddinName.Size = new System.Drawing.Size(103, 20);
+            this.lbAddinName.TabIndex = 0;
+            this.lbAddinName.Text = "ViewSetMaker";
+            // 
+            // txtTemplateName
+            // 
+            this.txtTemplateName.Location = new System.Drawing.Point(3, 69);
+            this.txtTemplateName.Name = "txtTemplateName";
+            this.txtTemplateName.Size = new System.Drawing.Size(229, 21);
+            this.txtTemplateName.TabIndex = 1;
+            // 
+            // lbTemplate
+            // 
+            this.lbTemplate.AutoSize = true;
+            this.lbTemplate.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbTemplate.Location = new System.Drawing.Point(3, 46);
+            this.lbTemplate.Name = "lbTemplate";
+            this.lbTemplate.Size = new System.Drawing.Size(61, 17);
+            this.lbTemplate.TabIndex = 2;
+            this.lbTemplate.Text = "Template";
+            // 
+            // btNewSet
+            // 
+            this.btNewSet.Location = new System.Drawing.Point(3, 96);
+            this.btNewSet.Name = "btNewSet";
+            this.btNewSet.Size = new System.Drawing.Size(43, 23);
+            this.btNewSet.TabIndex = 3;
+            this.btNewSet.Text = "New";
+            this.btNewSet.UseVisualStyleBackColor = true;
+            // 
+            // btLoadSet
+            // 
+            this.btLoadSet.Location = new System.Drawing.Point(47, 96);
+            this.btLoadSet.Name = "btLoadSet";
+            this.btLoadSet.Size = new System.Drawing.Size(43, 23);
+            this.btLoadSet.TabIndex = 4;
+            this.btLoadSet.Text = "Load";
+            this.btLoadSet.UseVisualStyleBackColor = true;
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(122, 96);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(45, 23);
+            this.btSave.TabIndex = 5;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            // 
+            // btSaveAs
+            // 
+            this.btSaveAs.Location = new System.Drawing.Point(168, 96);
+            this.btSaveAs.Name = "btSaveAs";
+            this.btSaveAs.Size = new System.Drawing.Size(64, 23);
+            this.btSaveAs.TabIndex = 6;
+            this.btSaveAs.Text = "Save As";
+            this.btSaveAs.UseVisualStyleBackColor = true;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(3, 350);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(229, 118);
+            this.txtStatus.TabIndex = 7;
+            // 
+            // brProgress
+            // 
+            this.brProgress.Location = new System.Drawing.Point(3, 474);
+            this.brProgress.Name = "brProgress";
+            this.brProgress.Size = new System.Drawing.Size(229, 23);
+            this.brProgress.TabIndex = 8;
+            this.brProgress.Tag = "";
+            // 
+            // numBuildingStart
+            // 
+            this.numBuildingStart.Location = new System.Drawing.Point(62, 172);
+            this.numBuildingStart.Multiline = true;
+            this.numBuildingStart.Name = "numBuildingStart";
+            this.numBuildingStart.Size = new System.Drawing.Size(52, 21);
+            this.numBuildingStart.TabIndex = 9;
+            // 
+            // lbDongNum
+            // 
+            this.lbDongNum.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbDongNum.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbDongNum.Location = new System.Drawing.Point(5, 152);
+            this.lbDongNum.Name = "lbDongNum";
+            this.lbDongNum.Size = new System.Drawing.Size(54, 17);
+            this.lbDongNum.TabIndex = 10;
+            this.lbDongNum.Text = "동 번호";
+            // 
+            // lbBuildingStart
+            // 
+            this.lbBuildingStart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbBuildingStart.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbBuildingStart.Location = new System.Drawing.Point(3, 172);
+            this.lbBuildingStart.Name = "lbBuildingStart";
+            this.lbBuildingStart.Size = new System.Drawing.Size(48, 21);
+            this.lbBuildingStart.TabIndex = 11;
+            this.lbBuildingStart.Text = "시작";
+            this.lbBuildingStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbBuildingEnd
+            // 
+            this.lbBuildingEnd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbBuildingEnd.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbBuildingEnd.Location = new System.Drawing.Point(125, 170);
+            this.lbBuildingEnd.Name = "lbBuildingEnd";
+            this.lbBuildingEnd.Size = new System.Drawing.Size(49, 23);
+            this.lbBuildingEnd.TabIndex = 13;
+            this.lbBuildingEnd.Text = "끝";
+            this.lbBuildingEnd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numBuildingEnd
+            // 
+            this.numBuildingEnd.Location = new System.Drawing.Point(180, 170);
+            this.numBuildingEnd.Multiline = true;
+            this.numBuildingEnd.Name = "numBuildingEnd";
+            this.numBuildingEnd.Size = new System.Drawing.Size(52, 23);
+            this.numBuildingEnd.TabIndex = 12;
+            // 
+            // lbStandardFloor
+            // 
+            this.lbStandardFloor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbStandardFloor.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbStandardFloor.Location = new System.Drawing.Point(125, 224);
+            this.lbStandardFloor.Name = "lbStandardFloor";
+            this.lbStandardFloor.Size = new System.Drawing.Size(49, 21);
+            this.lbStandardFloor.TabIndex = 18;
+            this.lbStandardFloor.Text = "기준층";
+            this.lbStandardFloor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numStandardFloor
+            // 
+            this.numStandardFloor.Location = new System.Drawing.Point(180, 224);
+            this.numStandardFloor.Multiline = true;
+            this.numStandardFloor.Name = "numStandardFloor";
+            this.numStandardFloor.Size = new System.Drawing.Size(52, 21);
+            this.numStandardFloor.TabIndex = 17;
+            // 
+            // lbEntranceFloor
+            // 
+            this.lbEntranceFloor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbEntranceFloor.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbEntranceFloor.Location = new System.Drawing.Point(2, 224);
+            this.lbEntranceFloor.Name = "lbEntranceFloor";
+            this.lbEntranceFloor.Size = new System.Drawing.Size(49, 21);
+            this.lbEntranceFloor.TabIndex = 16;
+            this.lbEntranceFloor.Text = "출입구";
+            this.lbEntranceFloor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbPlanPositionFloor
+            // 
+            this.lbPlanPositionFloor.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbPlanPositionFloor.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbPlanPositionFloor.Location = new System.Drawing.Point(3, 203);
+            this.lbPlanPositionFloor.Name = "lbPlanPositionFloor";
+            this.lbPlanPositionFloor.Size = new System.Drawing.Size(95, 17);
+            this.lbPlanPositionFloor.TabIndex = 15;
+            this.lbPlanPositionFloor.Text = "도면 배치 위치";
+            // 
+            // numEntranceFloor
+            // 
+            this.numEntranceFloor.Location = new System.Drawing.Point(62, 224);
+            this.numEntranceFloor.Multiline = true;
+            this.numEntranceFloor.Name = "numEntranceFloor";
+            this.numEntranceFloor.Size = new System.Drawing.Size(52, 21);
+            this.numEntranceFloor.TabIndex = 14;
+            // 
+            // lbUndergroundFloorNum
+            // 
+            this.lbUndergroundFloorNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbUndergroundFloorNum.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbUndergroundFloorNum.Location = new System.Drawing.Point(3, 279);
+            this.lbUndergroundFloorNum.Name = "lbUndergroundFloorNum";
+            this.lbUndergroundFloorNum.Size = new System.Drawing.Size(49, 21);
+            this.lbUndergroundFloorNum.TabIndex = 21;
+            this.lbUndergroundFloorNum.Text = "최하층";
+            this.lbUndergroundFloorNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbPlanPositionFloorUnderground
+            // 
+            this.lbPlanPositionFloorUnderground.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbPlanPositionFloorUnderground.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbPlanPositionFloorUnderground.Location = new System.Drawing.Point(3, 257);
+            this.lbPlanPositionFloorUnderground.Name = "lbPlanPositionFloorUnderground";
+            this.lbPlanPositionFloorUnderground.Size = new System.Drawing.Size(105, 17);
+            this.lbPlanPositionFloorUnderground.TabIndex = 20;
+            this.lbPlanPositionFloorUnderground.Text = "지하주차장 층수";
+            // 
+            // numUndergroundNum
+            // 
+            this.numUndergroundNum.Location = new System.Drawing.Point(62, 279);
+            this.numUndergroundNum.Multiline = true;
+            this.numUndergroundNum.Name = "numUndergroundNum";
+            this.numUndergroundNum.Size = new System.Drawing.Size(52, 21);
+            this.numUndergroundNum.TabIndex = 19;
+            // 
+            // btRun
+            // 
+            this.btRun.Location = new System.Drawing.Point(3, 321);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(229, 23);
+            this.btRun.TabIndex = 23;
+            this.btRun.Text = "Run";
+            this.btRun.UseVisualStyleBackColor = true;
+            // 
+            // lbDeveloperName
+            // 
+            this.lbDeveloperName.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lbDeveloperName.Location = new System.Drawing.Point(149, 5);
+            this.lbDeveloperName.Name = "lbDeveloperName";
+            this.lbDeveloperName.Size = new System.Drawing.Size(83, 17);
+            this.lbDeveloperName.TabIndex = 24;
+            this.lbDeveloperName.Text = "By Keeleys95";
+            // 
+            // UcColtrol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbDeveloperName);
+            this.Controls.Add(this.btRun);
+            this.Controls.Add(this.lbUndergroundFloorNum);
+            this.Controls.Add(this.lbPlanPositionFloorUnderground);
+            this.Controls.Add(this.numUndergroundNum);
+            this.Controls.Add(this.lbStandardFloor);
+            this.Controls.Add(this.numStandardFloor);
+            this.Controls.Add(this.lbEntranceFloor);
+            this.Controls.Add(this.lbPlanPositionFloor);
+            this.Controls.Add(this.numEntranceFloor);
+            this.Controls.Add(this.lbBuildingEnd);
+            this.Controls.Add(this.numBuildingEnd);
+            this.Controls.Add(this.lbBuildingStart);
+            this.Controls.Add(this.lbDongNum);
+            this.Controls.Add(this.numBuildingStart);
+            this.Controls.Add(this.brProgress);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btSaveAs);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.btLoadSet);
+            this.Controls.Add(this.btNewSet);
+            this.Controls.Add(this.lbTemplate);
+            this.Controls.Add(this.txtTemplateName);
+            this.Controls.Add(this.lbAddinName);
+            this.Name = "UcColtrol";
+            this.Size = new System.Drawing.Size(235, 500);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbAddinName;
+        private System.Windows.Forms.TextBox txtTemplateName;
+        private System.Windows.Forms.Label lbTemplate;
+        private System.Windows.Forms.Button btNewSet;
+        private System.Windows.Forms.Button btLoadSet;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btSaveAs;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.ProgressBar brProgress;
+        private System.Windows.Forms.TextBox numBuildingStart;
+        private System.Windows.Forms.Label lbDongNum;
+        private System.Windows.Forms.Label lbBuildingStart;
+        private System.Windows.Forms.Label lbBuildingEnd;
+        private System.Windows.Forms.TextBox numBuildingEnd;
+        private System.Windows.Forms.Label lbStandardFloor;
+        private System.Windows.Forms.TextBox numStandardFloor;
+        private System.Windows.Forms.Label lbEntranceFloor;
+        private System.Windows.Forms.Label lbPlanPositionFloor;
+        private System.Windows.Forms.TextBox numEntranceFloor;
+        private System.Windows.Forms.Label lbUndergroundFloorNum;
+        private System.Windows.Forms.Label lbPlanPositionFloorUnderground;
+        private System.Windows.Forms.TextBox numUndergroundNum;
+        private System.Windows.Forms.Button btRun;
+        private System.Windows.Forms.Label lbDeveloperName;
+    }
+}
